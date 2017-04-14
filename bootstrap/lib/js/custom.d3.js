fb996e1ff2997ab4ad333b35025f7711ab657cba
Liu Cong <1434580150@qq.com>
2017-04-14 11:22:26 +0800

    add processing

--- bootstrap/lib/js/custom.d3.js
+++ bootstrap/lib/js/custom.d3.js
@@ -7,10 +7,10 @@ var margin = { top: 50, right: 10, bottom: 100, left: 30 },
 				buckets = 8,
 				colors = ["#ffffe3","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"],
 				frequency = ["200", "160", "120", "80", "40", "0"],
-				days = ["5.11", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "11.11"];
-				datasets = ["data_converted.csv"];
+				days = ["5.11", "5.26", "6.11", "6.26", "7.11", "7.26", "8.11", "8.26", "9.11", "9.26", "10.11", "10.26", "11.11"];
+				datasets = ["data_converted.csv","data_converted_2.csv"];
 
-			var svg = d3.select("#chart").append("svg")
+			var svg = d3.select("#chart_1").append("svg")
 				.attr("width", width + margin.left + margin.right)
 				.attr("height", height + margin.top + margin.bottom)
 				.append("g")
@@ -20,9 +20,12 @@ var margin = { top: 50, right: 10, bottom: 100, left: 30 },
 				.data(frequency)
 				.enter().append("text")
 				.text(function (d) { return d; })
-				.attr("x", 0)
+				.attr("x", -6)
 				.attr("y", function (d, i) { return i * gridSizeHeight*40; })
 				.style("text-anchor", "end")
+				.style("font-size", "9pt")
+				.style("font-family", "Consolas, courier")
+				.style("fill", "#aaa")
 				.attr("transform", "translate(-6," + gridSizeWidth / 1.5 + ")")
 				.attr("class", function (d, i) { 
 					return "frequencyLabel mono axis axis-workweek"; 
@@ -32,9 +35,12 @@ var margin = { top: 50, right: 10, bottom: 100, left: 30 },
 				.data(days)
 				.enter().append("text")
 				.text(function(d) { return d; })
-				.attr("x", function(d, i) { return i * gridSizeWidth*8; })
+				.attr("x", function(d, i) { return i * gridSizeWidth*15.5; })
 				.attr("y", height+ margin.top)
 				.style("text-anchor", "middle")
+				.style("font-size", "9pt")
+				.style("font-family", "Consolas, courier")
+				.style("fill", "#aaa")
 				.attr("transform", "translate(" + gridSizeWidth / 2 + ", -6)")
 				.attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "dayLabel mono axis axis-workday" : "dayLabel mono axis"); });
 
@@ -71,11 +77,13 @@ var margin = { top: 50, right: 10, bottom: 100, left: 30 },
 						.attr("width", gridSizeWidth)
 						.attr("height", gridSizeHeight)
 						.style("fill", colors[0])
+						.style("stroke", "white")
+					    .style("stroke-width", "0.4px") 
 						.on("mouseover", function(){return tooltip.style("visibility", "visible");})
 						.on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
-						.on("mouseout", function(){return tooltip.style("visibility", "hidden");});;;
+						.on("mouseout", function(){return tooltip.style("visibility", "hidden");});
 
-					cards.transition().duration(2000)
+					cards.transition().duration(500)
 						.style("fill", function(d) { return colorScale(d.value); })
 
 
@@ -84,4 +92,4 @@ var margin = { top: 50, right: 10, bottom: 100, left: 30 },
 				})
 			};
 
-			heatmapChart(datasets[0]);
+			heatmapChart(datasets[1]);
